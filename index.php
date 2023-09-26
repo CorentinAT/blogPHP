a36480a846f67f2e14f6679c398f335e7a87061b
Risson <aime.risson.1@gmail.com>
2023-09-26 17:04:00 +0200

    structure base

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require("structure/header.php"); ?>
+<h1 class="text-2xl ">BLOG</h1>
+<?php require("structure/footer.php"); ?>
---
+++ structure/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title><\?php blog?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
