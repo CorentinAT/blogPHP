97ee76c279cc623aaba35b6572f833f9aa835787
CorentinAT <corentin.al2004@gmail.com>
2023-09-26 17:48:24 +0200

    fonction nvarticle dans sql.php

--- traitement/sql.php
+++ traitement/sql.php
@@ -4,4 +4,20 @@
     header('Location: index.php');
     exit();
   }
+
+  function nvarticle($titre, $description, $categories, $id_user) {
+    $sql = $connexion->prepare("INSERT INTO article (titre, description, id_user) VALUES (?, ?, ?)");
+    $sql->execute([$titre, $description, $id_user]);
+    $id_article = $connexion->lastInsertId();
+
+    foreach($categories as $categorie) {
+      $sql = $connexion->prepare("INSERT INTO lien_categorie_article (id_article, id_categorie) VALUES (?, ?)");
+      $sql->execute([$id_article, $categorie]);
+    }
+  }
+
+  switch($_POST['form_sql']) {
+    case "nvarticle":
+      nvarticle($_POST['titre'], $_POST['description'], $_SESSION['id_user'], $_POST['categories']);
+  }
 ?>
