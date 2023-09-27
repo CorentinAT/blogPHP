7a75247ab924bde2b3d66ed429bdf50ca978261f
CorentinAT <corentin.al2004@gmail.com>
2023-09-27 14:44:24 +0200

    gestion différentes erreurs dans création darticle

--- traitement/traitement_forms.php
+++ traitement/traitement_forms.php
@@ -10,12 +10,18 @@
   switch($_POST['form_name']) {
     case 'nvarticle':
       require 'sql.php';
-      if(isset($_POST['titre']) && isset($_POST['description']) && isset($_SESSION['id_user'])) {
+      if(!empty($_POST['titre']) && !empty($_POST['description']) && isset($_SESSION['id_user'])) {
         nvarticle($_POST['titre'], $_POST['description'], $_SESSION['id_user'], $_POST['categories']);
         header("Location: http://localhost/index.php");
         exit(0);
       } else {
-        $_SESSION['error_article'] = "article non valide";
+        if(empty($_POST['titre'])) {
+          $_SESSION['error_article'] = "Veuillez mettre un titre";
+        } else if(empty($_POST['description'])) {
+          $_SESSION['error_article'] = "Veuillez entrer un article valide";
+        } else {
+          $_SESSION['error_article'] = "Erreur dans la publication de l'article";
+        }
         header("Location: http://localhost/pages/creer_article.php");
         exit(0);
       }
