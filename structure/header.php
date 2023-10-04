49dd43348bc63f99c7069bb33dd60d95f01e2821
CorentinAT <corentin.al2004@gmail.com>
2023-10-04 17:54:18 +0200

    header avec connexion et deconnexion

--- structure/header.php
+++ structure/header.php
@@ -7,16 +7,16 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-screen">
-  <div class="w-screen flex justify-between bg-gray-100 p-2 items-center mb-4">
-    <h1 class="text-2xl"><\?php BLOG?></h1>
-    <div>
-      <a href="/pages/creer_article.php" class="text-blue-500 hover:underline">creer article</a>
+  <div class="w-screen flex justify-between bg-gray-100 py-2 px-4 items-center mb-4">
+    <a href="/index.php"><h1 class="text-2xl"><\?php BLOG?></h1></a>
+    <div class="flex gap-4">
+      <a href="/pages/creer_article.php" class="text-blue-500 hover:underline">Nouvel article</a>
       <?php
         session_start();
         if(isset($_SESSION['id_user'])) {
-          echo "<a class='text-blue-500 hover:underline'>Déconnexion</a>";
+          echo "<a href='/traitement/deconnexion.php' class='text-blue-500 hover:underline'>Déconnexion</a>";
         } else {
-          echo "<a class='text-blue-500 hover:underline'>Connexion</a>";
+          echo "<a href='/pages/login.php' class='text-blue-500 hover:underline'>Connexion</a>";
         }
       ?>
     </div>
