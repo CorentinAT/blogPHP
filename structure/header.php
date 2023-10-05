a48168dfcc7de74e10c9907742f96a5889102b98
CorentinAT <corentin.al2004@gmail.com>
2023-10-05 13:24:54 +0200

    changement page admin

--- structure/header.php
+++ structure/header.php
@@ -7,14 +7,14 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-screen">
-  <div class="w-screen flex justify-between bg-gray-100 py-2 px-4 items-center mb-4">
+  <div class="w-screen flex justify-between bg-gray-100 py-2 px-4 items-center mb-4 fixed">
     <a href="/index.php"><h1 class="text-2xl"><\?php BLOG?></h1></a>
     <div class="flex gap-4">
       <a href="/pages/creer_article.php" class="text-blue-500 hover:underline">Nouvel article</a>
       <?php
         session_start();
         if(isset($_SESSION['id_user']) && $_SESSION['is_admin']==1) {
-          echo "<a href='/pages/creer_categorie.php' class='text-blue-500 hover:underline'>Nouvelle catégorie</a>";
+          echo "<a href='/pages/admin.php' class='text-blue-500 hover:underline'>Admin</a>";
         }
         if(isset($_SESSION['id_user'])) {
           echo "<a href='/traitement/deconnexion.php' class='text-blue-500 hover:underline'>Déconnexion</a>";
